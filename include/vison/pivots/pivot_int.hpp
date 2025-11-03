942265f39c80dd3423e0ceb6b64d62cce182bda7
Julien <pgyygp090@gmail.com>
2025-11-03 16:42:27 +0100

    updated pivot_int to support custom Int

--- include/vison/pivots/pivot_int.hpp
+++ include/vison/pivots/pivot_int.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
-void pivot_int(Dataframe &obj, unsigned int &n1, unsigned int& n2, unsigned int& n3) {
+void pivot_int(Dataframe &obj, 
+                unsigned int &n1, 
+                unsigned int& n2, 
+                unsigned int& n3) 
+{
   const std::vector<std::vector<std::string>>& tmp = obj.get_tmp_val_refv();
   const std::vector<std::string>& col_vec = tmp[n1];
   const std::vector<std::string>& row_vec = tmp[n2];
@@ -10,10 +14,10 @@ void pivot_int(Dataframe &obj, unsigned int &n1, unsigned int& n2, unsigned int&
   unsigned int pos_val;
   const std::vector<int>& cur_int_v = obj.get_int_vec();
 
-  std::vector<int> tmp_int_v = {};
+  std::vector<IntT> tmp_int_v = {};
 
   //std::unordered_map<std::pair<std::string_view, std::string_view>, int, PairHash> lookup; // standard map (slower)
-  ankerl::unordered_dense::map<std::pair<std::string_view, std::string_view>, int, PairHash> lookup;
+  ankerl::unordered_dense::map<std::pair<std::string_view, std::string_view>, IntT, PairHash> lookup;
 
   std::string key;
   for (auto& el : matr_idx2[3]) {
@@ -48,7 +52,7 @@ void pivot_int(Dataframe &obj, unsigned int &n1, unsigned int& n2, unsigned int&
   std::vector<std::string> cur_vec_str(idx_row.size());
   tmp_val_refv.resize(ncol, cur_vec_str);
  
-  int cur_int;      
+  IntT cur_int;      
   for (const auto& [key_v, value] : idx_col) {
     for (const auto& [key_v2, value2] : idx_row) {
       auto key_pair = std::pair<std::string_view, std::string_view> {key_v, key_v2};
