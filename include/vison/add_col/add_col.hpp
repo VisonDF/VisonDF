e90836656e128a5ba76460c4070da78215dd6e04
Julien <pgyygp090@gmail.com>
2025-11-06 18:23:49 +0100

    optimized add_col

--- include/vison/add_col/add_col.hpp
+++ include/vison/add_col/add_col.hpp
@@ -1,61 +1,219 @@
 #pragma once
 
-template <typename T> void add_col(std::vector<T> &x, std::string name = "NA") {
-  if (x.size() != nrow) {
-    std::cerr << "Error: vector length (" << x.size()
-              << ") does not match nrow (" << nrow << ")\n";
-    return;
-  };
-  name_v.push_back(name);
-  std::vector<std::string> cur_v = {};
-  cur_v.reserve(nrow);
-  if constexpr (std::is_same_v<T, bool>) {
-    matr_idx[2].push_back(ncol);
-    type_refv.push_back('b');
-    for (auto &i : x) {
-      bool_v.push_back(i);
-      cur_v.push_back(std::to_string(i));
+template <typename T, bool Large = 1> 
+void add_col(std::vector<T> &x, std::string name = "NA") {
+  
+    if (x.size() != nrow) {
+      std::cerr << "Error: vector length (" << x.size()
+                << ") does not match nrow (" << nrow << ")\n";
+      return;
     };
-  } else if constexpr (std::is_same_v<T, IntT>) {
-    matr_idx[3].push_back(ncol);
-    type_refv.push_back('i');
-    for (auto &i : x) {
-      int_v.push_back(i);
-      cur_v.push_back(std::to_string(i));
+    
+    name_v.push_back(name);
+    tmp_val_refv.resize(tmp_val_refv.size() + 1);
+    auto& val_tmp = tmp_val_refv.back();
+    val_tmp.resize(nrow);
+
+    if constexpr (std::is_same_v<T, bool>) {
+
+      matr_idx[2].push_back(ncol);
+      type_refv.push_back('b');
+      
+      const size_t base_idx = bool_v.size();
+      bool_v.resize(base_idx + nrow);
+    
+      const size_t buf_size = 2;
+      size_t i;
+
+      for (auto& el : val_tmp) {
+        el.reserve(buf_size);
+      }
+
+      for (i = 0; i < nrow; i += 1) {
+          bool_v[base_idx + i] = x[i];
+      };
+
+      for (i = 0; i < nrow; i += 1) {
+          char buf[buf_size];
+          auto [ptr, ec] = std::to_chars(buf, buf + buf_size, 
+                                         static_cast<int>(x[i]));
+
+          if (ec == std::errc{}) [[likely]] {
+              val_tmp[i].assign(buf, ptr);
+          } else [[unlikely]] {
+              std::terminate();
+          }
+      }
+
+    } else if constexpr (std::is_same_v<T, IntT>) {
+
+      matr_idx[3].push_back(ncol);
+      type_refv.push_back('i');
+
+      const size_t base_idx = int_v.size();
+      int_v.resize(base_idx + nrow);
+    
+      constexpr size_t buf_size = max_chars_needed<T>();
+      size_t i;
+      auto* __restrict dst = std::assume_aligned<64>(int_v.data() + base_idx);
+      auto* __restrict src = std::assume_aligned<64>(x.data());
+
+      for (auto& el : val_tmp) {
+        el.reserve(buf_size);
+      }
+
+      #pragma GCC ivdep
+      for (i = 0; i < nrow; i += 1) {
+          dst[i] = src[i];
+      };
+
+      for (i = 0; i < nrow; i += 1) {
+          char buf[buf_size];
+          auto [ptr, ec] = std::to_chars(buf, buf + buf_size, x[i]);
+
+          if (ec == std::errc{}) [[likely]] {
+              val_tmp[i].assign(buf, ptr);
+          } else [[unlikely]] {
+              std::terminate();
+          }
+      }
+
+    } else if constexpr (std::is_same_v<T, UIntT>) {
+
+      matr_idx[4].push_back(ncol);
+      type_refv.push_back('u');
+ 
+      const size_t base_idx = uint_v.size();
+      uint_v.resize(base_idx + nrow);
+    
+      constexpr size_t buf_size = max_chars_needed<T>();
+      size_t i;
+      auto* __restrict dst = std::assume_aligned<64>(uint_v.data() + base_idx);
+      auto* __restrict src = std::assume_aligned<64>(x.data());
+
+      for (auto& el : val_tmp) {
+        el.reserve(buf_size);
+      }
+
+      #pragma GCC ivdep
+      for (i = 0; i < nrow; i += 1) {
+          dst[i] = src[i];
+      };
+
+      for (i = 0; i < nrow; i += 1) {
+          char buf[buf_size];
+          auto [ptr, ec] = std::to_chars(buf, buf + buf_size, x[i]);
+
+          if (ec == std::errc{}) [[likely]] {
+              val_tmp[i].assign(buf, ptr);
+          } else [[unlikely]] {
+              std::terminate();
+          }
+      }
+      
+    } else if constexpr (std::is_same_v<T, FloatT>) {
+
+      matr_idx[5].push_back(ncol);
+      type_refv.push_back('d');
+
+      const size_t base_idx = dbl_v.size();
+      dbl_v.resize(base_idx + nrow);
+    
+      constexpr size_t buf_size = max_chars_needed<T>();
+      size_t i;
+      auto* __restrict dst = std::assume_aligned<64>(dbl_v.data() + base_idx);
+      auto* __restrict src = std::assume_aligned<64>(x.data());
+
+      for (auto& el : val_tmp) {
+        el.reserve(buf_size);
+      }
+
+      #pragma GCC ivdep
+      for (i = 0; i < nrow; i += 1) {
+          dst[i] = src[i];
+      };
+
+      for (i = 0; i < nrow; i += 1) {
+          char buf[buf_size];
+          auto [ptr, ec] = std::to_chars(buf, buf + buf_size, x[i]);
+
+          if (ec == std::errc{}) [[likely]] {
+              val_tmp[i].assign(buf, ptr);
+          } else [[unlikely]] {
+              std::terminate();
+          }
+      }
+
+    } else if constexpr (std::is_same_v<T, char>) {
+
+      matr_idx[1].push_back(ncol);
+      type_refv.push_back('c');
+
+      const size_t base_idx = chr_v.size();
+      chr_v.resize(base_idx + nrow);
+     
+      if constexpr (Large) {
+
+        size_t i;
+        auto* __restrict dst = std::assume_aligned<64>(chr_v.data() + base_idx);
+        auto* __restrict src = std::assume_aligned<64>(x.data());
+
+        #pragma GCC ivdep
+        for (i = 0; i < nrow; i += 1) {
+          dst[i] = src[i];
+        };
+
+        #pragma GCC ivdep
+        for (i = 0; i < nrow; i += 1) {
+          val_tmp[i].assign(1, x[i]);
+        }
+
+      } else if constexpr (!Large) {
+ 
+        for (size_t i = 0; i < nrow; i += 1) {
+          const auto& val_tmp2 = x[i];
+          chr_v[base_idx + i] = val_tmp2;
+          val_tmp[i].assign(1, val_tmp2);
+        }; 
+
+      }
+
+    } else if constexpr (std::is_same_v<T, std::string>) {
+
+      matr_idx[0].push_back(ncol);
+      type_refv.push_back('s');
+     
+      const size_t base_idx = str_v.size();
+      str_v.resize(base_idx + nrow);
+     
+      if constexpr (Large) {
+
+        size_t i;
+
+        for (i = 0; i < nrow; i += 1) {
+          str_v[base_idx + i] = x[i];
+        };
+
+        for (i = 0; i < nrow; i += 1) {
+          val_tmp[i] = x[i];
+        }
+
+      } else if constexpr (!Large) {
+ 
+        for (size_t i = 0; i < nrow; i += 1) {
+          const auto& val_tmp2 = x[i];
+          str_v[base_idx + i] = val_tmp2;
+          val_tmp[i] = val_tmp2;
+        }; 
+
+      }
+
+    } else {
+      std::cerr << "Error in (add_col) type not suported \n";
     };
-  } else if constexpr (std::is_same_v<T, UIntT>) {
-    matr_idx[4].push_back(ncol);
-    type_refv.push_back('u');
-    for (auto &i : x) {
-      uint_v.push_back(i);
-      cur_v.push_back(std::to_string(i));
-    };
-  } else if constexpr (std::is_same_v<T, FloatT>) {
-    matr_idx[5].push_back(ncol);
-    type_refv.push_back('d');
-    for (auto &i : x) {
-      dbl_v.push_back(i);
-      cur_v.push_back(std::to_string(i));
-    }; 
-  } else if constexpr (std::is_same_v<T, char>) {
-    matr_idx[1].push_back(ncol);
-    type_refv.push_back('c');
-    for (auto &i : x) {
-      chr_v.push_back(i);
-      cur_v.push_back(std::string(1, i));
-    }; 
-  } else if constexpr (std::is_same_v<T, std::string>) {
-    matr_idx[0].push_back(ncol);
-    type_refv.push_back('s');
-    for (auto &i : x) {
-      str_v.push_back(i);
-      cur_v.push_back(i);
-    }; 
-  } else {
-    std::cerr << "Error in (add_col) type not suported \n";
-  };
-  tmp_val_refv.push_back(cur_v);
-  ncol += 1;
+
+    ncol += 1;
 };
+
 
 
