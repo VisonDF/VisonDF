99904657c8b6fe12021fbeda86e64c1acaf580e9
Julien <pgyygp090@gmail.com>
2025-11-07 20:20:40 +0100

    optimized rm_row func

--- include/vison/rm_row/rm_row.hpp
+++ include/vison/rm_row/rm_row.hpp
@@ -6,14 +6,6 @@ void rm_row(unsigned int x)
 
     const size_t old_nrow = nrow;
 
-    auto compact_block = [&](auto& vec, size_t base) {
-        size_t idx = 0;
-        auto beg = vec.begin() + base;
-        auto end = beg + old_nrow;
-        auto it  = std::remove_if(beg, end, [&](auto&) mutable { return idx++ == x; });
-        vec.erase(it, end);
-    };
-
     for (size_t t = 0; t < 6; ++t) {
         
         const std::vector<unsigned int>& matr_tmp = matr_idx[t];
@@ -29,19 +21,16 @@ void rm_row(unsigned int x)
             const size_t base = cpos * old_nrow;
             
             switch (t) {
-                case 0: compact_block(str_v,  base); break;
-                case 1: compact_block(chr_v,  base); break;
-                case 2: compact_block(bool_v, base); break;
-                case 3: compact_block(int_v,  base); break;
-                case 4: compact_block(uint_v, base); break;
-                case 5: compact_block(dbl_v,  base); break;
+                case 0: str_v.erase(str_v.begin() + x); break;
+                case 1: chr_v.erase(chr_v.begin() + x); break;
+                case 2: bool_v.erase(bool_v.begin() + x); break;
+                case 3: int_v.erase(int_v.begin() + x); break;
+                case 4: uint_v.erase(uint_v.begin() + x); break;
+                case 5: dbl_v.erase(dbl_v.begin() + x); break;
             }
             
             auto& aux = tmp_val_refv[matr_tmp[cpos]];
-            size_t idx = 0;
-            auto it = std::remove_if(aux.begin(), aux.end(),
-                                     [&](auto&) mutable { return idx++ == x; });
-            aux.erase(it, aux.end());
+            aux.erase(aux.begin() + x);
             if constexpr (MemClean) {
               aux.shrink_to_fit();
             }
@@ -63,13 +52,9 @@ void rm_row(unsigned int x)
     nrow = old_nrow - 1; 
 
     if (!name_v_row.empty()) {
-        auto& aux = name_v_row;
-        size_t idx = 0;
-        auto it = std::remove_if(aux.begin(), aux.end(),
-                                 [&](auto&) mutable { return idx++ == x; });
-        aux.erase(it, aux.end());
+        name_v_row.erase(name_v_row.begin() + x);
         if constexpr (MemClean) {
-          aux.shrink_to_fit();
+          name_v_row.shrink_to_fit();
         }
     }
 
