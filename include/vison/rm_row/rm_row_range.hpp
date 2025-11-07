e8867b540caf2faa696cb2f10d7c1cc663ce13f6
Julien <pgyygp090@gmail.com>
2025-11-07 19:52:29 +0100

    added handler for name_v_row non empty case

--- include/vison/rm_row/rm_row_range.hpp
+++ include/vison/rm_row/rm_row_range.hpp
@@ -17,48 +17,114 @@ void rm_row_range(std::vector<unsigned int> x)
         vec.erase(it, end);
     };
 
-    for (size_t t = 0; t < 6; ++t) {
-        
-        const std::vector<unsigned int>& matr_tmp = matr_idx[t];
+    if (name_v_row.empty()) {
 
-        if (matr_tmp.empty()) {
-            continue;
-        }
+        for (size_t t = 0; t < 6; ++t) {
+            
+            const std::vector<unsigned int>& matr_tmp = matr_idx[t];
 
-        const size_t ncols_t = matr_tmp.size();
-        
-        for (size_t cpos = ncols_t; cpos-- > 0; ) {
-            
-            const size_t base = cpos * old_nrow;
-            
-            switch (t) {
-                case 0: compact_block(str_v,  base); break;
-                case 1: compact_block(chr_v,  base); break;
-                case 2: compact_block(bool_v, base); break;
-                case 3: compact_block(int_v,  base); break;
-                case 4: compact_block(uint_v, base); break;
-                case 5: compact_block(dbl_v,  base); break;
+            if (matr_tmp.empty()) {
+                continue;
             }
+
+            const size_t ncols_t = matr_tmp.size();
             
-            auto& aux = tmp_val_refv[matr_tmp[cpos]];
-            size_t idx = 0;
-            auto it = std::remove_if(aux.begin(), aux.end(),
-                                     [&](auto&) mutable { return !keep[idx++]; });
-            aux.erase(it, aux.end());
+            for (size_t cpos = ncols_t; cpos-- > 0; ) {
+                
+                const size_t base = cpos * old_nrow;
+                
+                switch (t) {
+                    case 0: compact_block(str_v,  base); break;
+                    case 1: compact_block(chr_v,  base); break;
+                    case 2: compact_block(bool_v, base); break;
+                    case 3: compact_block(int_v,  base); break;
+                    case 4: compact_block(uint_v, base); break;
+                    case 5: compact_block(dbl_v,  base); break;
+                }
+                
+                auto& aux = tmp_val_refv[matr_tmp[cpos]];
+                size_t idx = 0;
+                auto it = std::remove_if(aux.begin(), aux.end(),
+                                         [&](auto&) mutable { return !keep[idx++]; });
+                aux.erase(it, aux.end());
+                
+                if constexpr (MemClean) {
+                   aux.shrink_to_fit();
+                }
+                
+            }
+
             if constexpr (MemClean) {
-               aux.shrink_to_fit();
+                switch (t) {
+                    case 0: str_v.shrink_to_fit(); break;
+                    case 1: chr_v.shrink_to_fit(); break;
+                    case 2: bool_v.shrink_to_fit(); break;
+                    case 3: int_v.shrink_to_fit(); break;
+                    case 4: uint_v.shrink_to_fit(); break;
+                    case 5: dbl_v.shrink_to_fit(); break;
+                }
             }
+
         }
 
-        if constexpr (MemClean) {
-            switch (t) {
-                case 0: str_v.shrink_to_fit(); break;
-                case 1: chr_v.shrink_to_fit(); break;
-                case 2: bool_v.shrink_to_fit(); break;
-                case 3: int_v.shrink_to_fit(); break;
-                case 4: uint_v.shrink_to_fit(); break;
-                case 5: dbl_v.shrink_to_fit(); break;
+    } else {
+
+        for (size_t t = 0; t < 6; ++t) {
+            
+            const std::vector<unsigned int>& matr_tmp = matr_idx[t];
+
+            if (matr_tmp.empty()) {
+                continue;
             }
+
+            const size_t ncols_t = matr_tmp.size();
+            
+            for (size_t cpos = ncols_t; cpos-- > 0; ) {
+                
+                const size_t base = cpos * old_nrow;
+                
+                switch (t) {
+                    case 0: compact_block(str_v,  base); break;
+                    case 1: compact_block(chr_v,  base); break;
+                    case 2: compact_block(bool_v, base); break;
+                    case 3: compact_block(int_v,  base); break;
+                    case 4: compact_block(uint_v, base); break;
+                    case 5: compact_block(dbl_v,  base); break;
+                }
+                
+                auto& aux = tmp_val_refv[matr_tmp[cpos]];
+                size_t idx = 0;
+                auto it = std::remove_if(aux.begin(), aux.end(),
+                                         [&](auto&) mutable { return !keep[idx++]; });
+                aux.erase(it, aux.end());
+                
+                if constexpr (MemClean) {
+                   aux.shrink_to_fit();
+                }
+                
+                aux = name_v_row;
+                idx = 0;
+                it = std::remove_if(aux.begin(), aux.end(),
+                                         [&](auto&) mutable { return !keep[idx++]; });
+                aux.erase(it, aux.end());
+
+                if constexpr (MemClean) {
+                   aux.shrink_to_fit();
+                }
+
+            }
+
+            if constexpr (MemClean) {
+                switch (t) {
+                    case 0: str_v.shrink_to_fit(); break;
+                    case 1: chr_v.shrink_to_fit(); break;
+                    case 2: bool_v.shrink_to_fit(); break;
+                    case 3: int_v.shrink_to_fit(); break;
+                    case 4: uint_v.shrink_to_fit(); break;
+                    case 5: dbl_v.shrink_to_fit(); break;
+                }
+            }
+
         }
 
     }
