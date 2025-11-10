bd064a6b4408e39ea482722cf76e47978268674a
Julien <pgyygp090@gmail.com>
2025-11-10 10:48:56 +0100

    added rasform_filter_idx_mt

---
+++ include/vison/transform/transform_filter/transform_filter_idx_mt.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+template<unsigned int CORES = 4, bool MemClean = false>
+void transform_filter_idx_mt(std::vector<unsigned int>& mask) 
+{
+    
+    unsigned int nrow2 = nrow;
+
+    #pragma omp parallel num_threads(CORES)
+    {
+      
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[0].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[0][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        auto* col = str_v.data() + nrow2 * i2;
+        const auto col2 = std::vector<std::string>(str_v.data() + nrow * i2, 
+                        str_v.data() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          col[nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+ 
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[1].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[1][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        auto* col = chr_v.data() + nrow2 * i2;
+        const auto col2 = std::vector<char>(chr_v.data() + nrow * i2, 
+                        chr_v.data() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          col[nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[2].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[2][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        const auto col2 = std::vector<bool>(bool_v.begin() + nrow * i2, 
+                        bool_v.begin() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          bool_v[nrow2 * i2 + nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[3].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[3][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        auto* col = int_v.data() + nrow2 * i2;
+        const auto col2 = std::vector<bool>(bool_v.begin() + nrow * i2, 
+                        bool_v.begin() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          col[nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[4].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[4][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        auto* col = uint_v.data() + nrow2 * i2;
+        const auto col2 = std::vector<bool>(bool_v.begin() + nrow * i2, 
+                        bool_v.begin() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          col[nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+
+      #pragma omp for schedule(static) nowait
+      for (size_t i2 = 0 ; i2 < matr_idx[5].size(); i2 += 1) {
+        const unsigned int& pos_vl = matr_idx[5][i2];
+        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
+        const std::vector<std::string> val_tmp2 = val_tmp;
+        unsigned int nrow_local = 0;
+        auto* col = dbl_v.data() + nrow2 * i2;
+        const auto col2 = std::vector<bool>(bool_v.begin() + nrow * i2, 
+                        bool_v.begin() + nrow * (i2 + 1));
+        for (const auto& pos_idx : mask) {
+          val_tmp[nrow_local] = val_tmp2[pos_idx];
+          col[nrow_local] = col2[pos_idx];
+          nrow_local += 1;
+        }
+      };
+
+    }
+
+    if (!name_v_row.empty()) {
+      unsigned int nrow_local = 0;
+      std::vector<std::string> name_v_row2 = name_v_row;
+      for (const auto& pos_idx : mask) {
+        name_v_row[nrow_local] = name_v_row2[pos_idx];
+        nrow_local += 1;
+      }
+    }
+
+    if constexpr (MemClean) {
+      for (auto& el : tmp_val_refv) {
+        el.shrink_to_fit();
+      }
+      
+      str_v.shrink_to_fit();
+      chr_v.shrink_to_fit();
+      bool_v.shrink_to_fit();
+      int_v.shrink_to_fit();
+      uint_v.shrink_to_fit();
+      dbl_v.shrink_to_fit();
+      name_v_row.shrink_to_fit();
+
+    }
+
+    nrow = std::count(mask.begin(), mask.end(), uint8_t{1});;
+
+};
+
+
+
+
+
