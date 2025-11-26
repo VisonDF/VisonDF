b26d821d5c799e9eec97dbac8e4b4c024d9152c7
Julien <pgyygp090@gmail.com>
2025-11-26 21:35:38 +0100

    better compile flags

--- benchmarks/readf_run.sh
+++ benchmarks/readf_run.sh
@@ -1,3 +1,3 @@
-g++ -O3 -std=c++20 -fopenmp -march=native -funroll-loops -ftree-vectorize -fvect-cost-model=dynamic -fno-rtti ../vison/vison.hpp
-g++ -O3 -std=c++20 -fopenmp -march=native -funroll-loops -ftree-vectorize -fvect-cost-model=dynamic -fno-rtti readf.cpp -o readf_bench
+g++ -Ofast -std=c++20 -march=native -mtune=native -fopenmp -fno-rtti ../include/vison/vison.hpp
+g++ -Ofast -std=c++20 -march=native -mtune=native -fopenmp -fno-rtti readf.cpp -o readf_bench
 ./readf_bench
