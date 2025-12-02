115e9ff520180e625acc7329c6244e9aa40e1c4f
Julien <pgyygp090@gmail.com>
2025-12-02 18:00:25 +0100

    factorized code

--- include/vison/transform/transform_filter/transform_filter_idx_mt.hpp
+++ include/vison/transform/transform_filter/transform_filter_idx_mt.hpp
@@ -5,23 +5,27 @@ template<unsigned int CORES = 4,
 void transform_filter_idx_mt(std::vector<unsigned int>& mask) 
 {
 
-   std::vector<uint8_t> used(nrow, 0);
-    for (unsigned x : mask) {
-        if (x < nrow) [[likely]]
-            used[x] = 1;
+    std::vector<uint8_t> used(nrow, 0);
+    #pragma omp paralel for num_threads(CORES)
+    for (size_t i = 0; i < mask.size(); ++i) {
+        if (mask[i] < nrow) [[likely]]
+            used[mask[i]] = 1;
     }
 
     std::vector<unsigned> to_delete;
-    to_delete.reserve(nrow + 1 - mask.size()); 
+    to_delete.resize(nrow + 1 - mask.size());
+    size_t i2 = 0;
     for (unsigned i = 0; i < nrow; ++i) {
-        if (!used[i])
-            to_delete.push_back(i);
+        if (!used[i]) {
+            to_delete[i2] = i;
+            i2 += 1;
+        }
     }
 
     if (to_delete.size() / nrow < 0.08) {
         rm_row_range_mt<CORES, MemClean>(to_delete);
     } else {
-        rm_row_range_reconstruct_mt<CORES, MemClean>(to_delete);
+        rm_row_range_reconstruct_mt<CORES>(to_delete);
     }
 
 };
--- include/vison/transform/transform_filter/transform_filter_range_mt.hpp
+++ include/vison/transform/transform_filter/transform_filter_range_mt.hpp
@@ -4,137 +4,23 @@ template<unsigned int CORES = 4, bool MemClean = false>
 void transform_filter_range_mt(const std::vector<uint8_t>& mask,
                                const unsigned int& strt_vl) 
 {
-    
-    unsigned int nrow2 = nrow;
 
-    #pragma omp parallel num_threads(CORES)
-    {
-      
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[0].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[0][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = str_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = std::move(val_tmp[i + strt_vl]);
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
- 
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[1].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[1][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = chr_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = val_tmp[i + strt_vl];
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
+    std::vector<unsigned int> to_delete;
+    to_delete.resize(mask.size());
 
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[2].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[2][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = bool_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = val_tmp[i + strt_vl];
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
-
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[3].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[3][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = int_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = val_tmp[i + strt_vl];
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
-
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[4].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[4][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = uint_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = val_tmp[i + strt_vl];
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
-
-      #pragma omp for schedule(static) nowait
-      for (size_t i2 = 0 ; i2 < matr_idx[5].size(); i2 += 1) {
-        const unsigned int& pos_vl = matr_idx[5][i2];
-        std::vector<std::string>& val_tmp = tmp_val_refv[pos_vl];
-        unsigned int nrow_local = 0;
-        auto* col = dbl_v.data() + nrow2 * i2;
-        for (size_t i = 0; i < mask.size(); ++i) {
-          if (!mask[i]) {
-            continue;
-          }
-          val_tmp[nrow_local] = val_tmp[i + strt_vl];
-          col[nrow_local] = col[i + strt_vl];
-          nrow_local += 1;
-        }
-      };
-
-    }
-
-    if (!name_v_row.empty()) {
-      unsigned int nrow_local = 0;
-      for (size_t i = 0; i < mask.size(); ++i) {
+    size_t i2 = 0;
+    for (size_t i = 0; i < mask.size(); ++i) {
         if (!mask[i]) {
-          continue;
+            to_delete[i2] = strt_vl + i;
+            i2 += 1;
         }
-        name_v_row[nrow_local] = name_v_row[i + strt_vl];
-        nrow_local += 1;
-      }
     }
 
-    if constexpr (MemClean) {
-      for (auto& el : tmp_val_refv) {
-        el.shrink_to_fit();
-      }
-      
-      str_v.shrink_to_fit();
-      chr_v.shrink_to_fit();
-      bool_v.shrink_to_fit();
-      int_v.shrink_to_fit();
-      uint_v.shrink_to_fit();
-      dbl_v.shrink_to_fit();
-      name_v_row.shrink_to_fit();
-
+    if (to_delete.size() / nrow < 0.08) {
+        rm_row_range_mt<CORES, MemClean>(to_delete);
+    } else {
+        rm_row_range_reconstruct_mt<CORES>(to_delete);
     }
-
-    nrow = std::count(mask.begin(), mask.end(), uint8_t{1});;
 
 };
 
