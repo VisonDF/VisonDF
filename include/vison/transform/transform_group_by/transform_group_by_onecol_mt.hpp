3172e49098ac1e6060c1d50f11893a48c6428c8e
Julien <pgyygp090@gmail.com>
2025-12-06 14:16:54 +0100

    no need to precise the type in eft_join now

---
+++ include/vison/transform/transform_group_by/transform_group_by_onecol_mt.hpp
@@ -0,0 +1,146 @@
+#pragma once
+
+template <unsigned int CORES = 4,
+          bool Occurence = false,
+          bool SimdHash = true>
+void transform_group_by_onecol_mt(unsigned int x,
+                                  const n_col int = -1,
+                                  const std::string colname = "n") 
+{
+
+    if constexpr (!Occurence) {
+        if (n_col < 0) {
+            std::cerr << "Can't take negative columns\n";
+            return;
+        } else if (n_col > ncol) {
+            std::cerr << "Column number out of range\n";
+            return;
+        }
+    }
+
+    using key_t = std::conditional_t<!std::is_same_v<T, void>, 
+                                     T*, 
+                                     std::variant<std::string*, 
+                                        CharT*, 
+                                        uint8_t*, 
+                                        IntT*, 
+                                        UIntT*, 
+                                        FloatT*>>;
+    using value_t = std::conditional_t<Occurence, 
+                                  unsigned int, 
+                                  std::variant<std::string, 
+                                        CharT, 
+                                        uint8_t, 
+                                        IntT, 
+                                        UIntT, 
+                                        FloatT>>;
+    using map_t = std::conditional_t<
+        SimdHash,
+        ankerl::unordered_dense::map<key_t, 
+                                     value_t, 
+                                     simd_hash>,
+        ankerl::unordered_dense::map<key_t, 
+                                     value_t>
+    >;
+
+    const unsigned int local_nrow = nrow;
+    std::vector<unsigned int> x2(1);
+    std::unordered_map<int, int> pos;
+    size_t idx_type;
+
+    using key_variant_t = std::variant<
+        std::nullptr_t,
+        const std::vector<std::vector<std::string>>*,
+        const std::vector<std::vector<CharT>>*,
+        const std::vector<std::vector<uint8_t>>*,
+        const std::vector<std::vector<IntT>>*,
+        const std::vector<std::vector<UIntT>>*,
+        const std::vector<std::vector<FloatT>>*
+    >;
+    
+    key_variant_t key_table = nullptr;
+    
+    if constexpr (!std::is_same_v<T, void>) {
+        if constexpr (std::is_same_v<T, std::string>) {
+            key_table = &str_v;
+            idx_type = 0;
+        } else if constexpr (std::is_same_v<T, CharT>) {
+            key_table = &chr_v;
+            idx_type = 1;
+        } else if constexpr (std::is_same_v<T, uint8_t>) {
+            key_table = &bool_v;
+            idx_type = 2;
+        } else if constexpr (std::is_same_v<T, IntT>) {
+            key_table = &int_v;
+            idx_type = 3;
+        } else if constexpr (std::is_same_v<T, UIntT>) {
+            key_table = &uint_v;
+            idx_type = 4;
+        } else if constexpr (std::is_same_v<T, FloatT>) {
+            key_table = &dbl_v;
+            idx_type = 5;
+        }
+    } else {
+        switch (type_refv[n_col]) {
+            case 's': key_table = &str_v;  break;
+            case 'c': key_table = &chr_v;  break;
+            case 'b': key_table = &bool_v; break;
+            case 'i': key_table = &int_v;  break;
+            case 'u': key_table = &uint_v; break;
+            case 'd': key_table = &dbl_v;  break;
+        }
+    }
+
+    for (int i = 0; i < matr_idx[idx_type].size(); ++i)
+        pos[matr_idx[idx_type][i]] = i;
+    const size_t real_pos = pos[x];
+
+    map_t lookup;
+    lookup.reserve(local_nrow);
+
+    std::vector<key_t*> key_vec(local_nrow);
+
+    for (unsigned int i = 0; i < local_nrow; ++i) {
+    
+        auto [it, inserted] = lookup.try_emplace(&(*key_table)[real_pos][i], 0);
+        if constexpr (Occurence) {
+            ++(it->second);
+        } else if constexpr (!Occurence) {
+            (it->second) += (*key_table)[n_col][i];
+        }
+    
+        key_vec[i] = &it->first;
+    }
+
+    std::vector<value_t> value_col(local_nrow);
+
+    #pragma omp parallel for if(CORES > 1) num_threads(CORES)
+    for (size_t i = 0; i < key_vec.size(); ++i) {
+        unsigned int count = lookup.at(*key_vec[i]);
+        value_col[i] = count;
+    }
+   
+    if constexpr (Ocurence) {
+        uint_v.push_back(value_col);
+    } else if (std::is_same_v<value_t, std:string>) {
+        str_v.push_back(value_col);
+    } else if (std:is_same_v<value_t, CharT>) {
+        chr_v.push_back(value_col);
+    } else if (std:is_same_v<value_t, uint8_t>) {
+        bool_v.push_back(value_col);
+    } else if (std::is_same_v<value_t, IntT>) {
+        int_v.push_back(value_col);
+    } else if (std::is_same_v<value_t, UIntT>) {
+        uint_v.push_back(value_col);
+    } else if (std::is_same_v<value_t, FloatT>) {
+        dbl_v.push_back(value_col);
+    }
+
+    if (!name_v.empty())
+        name_v.push_back(colname);
+
+    type_refv.push_back('u');
+    ++ncol;
+}
+
+
