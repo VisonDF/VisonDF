c327c0ecfb8e5cfeb6e4d54c426e4d228dc640ab
Julien <pgyygp090@gmail.com>
2025-12-09 15:00:01 +0100

    fixed boomask variant

--- include/vison/rm_row/rm_row_range_reconstruct_boolmask.hpp
+++ include/vison/rm_row/rm_row_range_reconstruct_boolmask.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 template <bool Sorted   = true,
-          bool MemClean = false>
+          bool MemClean = false,
+          bool Soft = true,
+          bool SanityCheck = true>
 void rm_row_range_reconstruct_boolmask(std::vector<unsigned int>& x,
                                        const size_t strt_vl)
 {
 
    rm_row_range_reconstruct_boolmask_mt<1, 
                                         Sorted, 
-                                        MemClean>(x, 
-                                                  strt_vl);
+                                        MemClean,
+                                        Soft,
+                                        SanityCheck>(x, 
+                                                     strt_vl);
 
 }
 
--- include/vison/rm_row/rm_row_range_reconstruct_boolmask_mt.hpp
+++ include/vison/rm_row/rm_row_range_reconstruct_boolmask_mt.hpp
@@ -2,8 +2,7 @@
 
 template <unsigned int CORES = 4,
           bool MemClean = false,
-          bool Soft = true,
-          bool SanityCheck = true>
+          bool Soft = true>
 void rm_row_range_reconstruct_boolmask_mt(std::vector<uint8_t>& x,
                                           const size_t strt_vl)
 {
@@ -21,64 +20,31 @@ void rm_row_range_reconstruct_boolmask_mt(std::vector<uint8_t>& x,
     if constexpr (Soft) {
 
         in_view = true;
-
         if (row_view_idx.empty())
             std::iota(row_view_idx.begin(), row_view_idx.end(), 0);
-
-        bool sort_assumption = Sorted;
-
-        if constexpr (!Sorted) {
-            if (x.size() > 1000) {
-                std::vector<uint8_t> is_active(nrow, 1);
-                for (auto& i : x) {
-                    if (i < old_nrow) [[likely]] {
-                        is_active[i] = 0;
-                    }
-                }
-                size_t i2 = 0;
-                for (size_t i = 0; i < old_nrow; ++i) {
-                    if (is_active[i]) {
-                        row_view_idx[i2] = row_view_idx[i];
-                        i2 += 1;
-                    }
-                }
-            } else {
-                std::sort(x.begin(), x.end());
-                if constexpr (SanityCheck) {
-                    x.erase(
-                        std::remove_if(x.begin(), x.end(),
-                                       [&](size_t v){ return v >= old_nrow; }),
-                        x.end()
-                    );
-                    x.erase(std::unique(x.begin(), x.end()), x.end());
-                }
-                sort_assumption = true;
-            }
-        } 
-
-        if (sort_assumption) {
-            size_t i = x[0] + 1;
-            size_t i2 = 1;
-            size_t written = x[0];
-            while (i2 < x.size()) {
-                unsigned ref_val = x[i2++];
-                const size_t start = i;
-                while (i < ref_val) ++i;
-                size_t len = i - start;
-                {
-                    T* __restrict d = row_view_idx.data() + written;
-                    T* __restrict s = row_view_idx.data() + start;
-                    
-                    std::memmove(d, s, len * sizeof(size_t));
-                } 
-                written += len;
-                i += 1;
-            }
-            while (i < old_nrow) {
-                row_view_idx[written] = row_view_idx[i];
-                i += 1;
-                written += 1;
-            };
+            
+        size_t i = 0;
+        size_t written = 0;
+        while (!x[i]) {
+            i += 1;
+            written += 1;
+        }
+        while (i < new_nrow && x[i]) {
+            i += 1;
+        }
+        while (i < x.size()) {
+            size_t start = i;
+            while (i < x.size() && !x[i]) ++i;
+            size_t len = i - start; 
+            {
+                T* __restrict d = row_view_idx.data() + written;
+                T* __restrict s = row_view_idx.data() + start;
+                
+                std::memmove(d, s, len * sizeof(size_t));
+            } 
+            written += len;
+            i += 1;
+        }
 
     } else {
 
