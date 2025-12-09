850d88bdeb6624dc776e25a46410ffc25b5c98cf
Julien <pgyygp090@gmail.com>
2025-12-09 15:06:52 +0100

    fixed rm_row_range_boolmak

--- include/vison/rm_row/rm_row_range_reconstruct_boolmask.hpp
+++ include/vison/rm_row/rm_row_range_reconstruct_boolmask.hpp
@@ -1,19 +1,15 @@
 #pragma once
 
-template <bool Sorted   = true,
-          bool MemClean = false,
-          bool Soft = true,
-          bool SanityCheck = true>
+template <bool MemClean = false,
+          bool Soft = true>
 void rm_row_range_reconstruct_boolmask(std::vector<unsigned int>& x,
                                        const size_t strt_vl)
 {
 
    rm_row_range_reconstruct_boolmask_mt<1, 
-                                        Sorted, 
                                         MemClean,
-                                        Soft,
-                                        SanityCheck>(x, 
-                                                     strt_vl);
+                                        Soft>(x, 
+                                              strt_vl);
 
 }
 
