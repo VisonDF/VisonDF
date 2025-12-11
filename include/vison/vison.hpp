4475ec4376fe9c0bf3b07b60957bb4f0a8b2cab2
Julien <pgyygp090@gmail.com>
2025-12-11 21:24:32 +0100

    updated includes

--- include/vison/vison.hpp
+++ include/vison/vison.hpp
@@ -59,7 +59,8 @@ namespace vison {
     #include "types/comparator.inl"
     #include "types/comparator_assertion.inl"
     #include "types/fapply_assertion.inl"
-    #include "types/group_by.inl"    
+    #include "types/group_by.inl"
+    #include "types/supported_group_methods.inl"    
 
     #include "inlines/inplace_permutation.inl"
     #include "inlines/no_inplace_permutation.inl"
