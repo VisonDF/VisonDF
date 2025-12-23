7efde2d555a9a9d08d412673aa94f9e3c94b554a
Julien <j.larget-piet@proton.me>
2025-12-23 17:48:24 +0100

    updated rm_row_range

--- include/vison/rm_row/rm_row_range.hpp
+++ include/vison/rm_row/rm_row_range.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
 template <bool MemClean = false,
-          bool Soft = true,
-          bool SanityCheck = true>
+          bool Soft = true>
 void rm_row_range(std::vector<unsigned int>& x) 
 {
 
     rm_row_range_mt<1, // CORES
                     MemClean,
-                    Soft,
-                    SanityCheck>(x);
+                    Soft>(x);
 
 };
 
