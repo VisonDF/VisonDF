d96b85d7d651520e9dff39a6744716f85b27e98b
Julien <j.larget-piet@proton.me>
2025-12-23 15:52:55 +0100

    optimized n_col_real creation

--- include/vison/transform/transform_group_by/soft/transform_group_by_onecol_soft_mt.hpp
+++ include/vison/transform/transform_group_by/soft/transform_group_by_onecol_soft_mt.hpp
@@ -73,8 +73,9 @@ void transform_group_by_onecol_soft_mt(unsigned int x)
     }
 
     std::unordered_map<int, int> pos;
+    const auto& cur_matr_idx = matr_idx[idx_type];
     for (int i = 0; i < matr_idx[idx_type].size(); ++i)
-        pos[matr_idx[idx_type][i]] = i;
+        pos[cur_matr_idx[i]] = i;
     const size_t real_pos = pos[x];
 
     map_t lookup;
