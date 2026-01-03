ae40c92dc4fa4f79c5def189b3bc432bbd8d958b
Julien <j.larget-piet@proton.me>
2026-01-03 17:10:28 +0100

    updated left_join

--- include/vison/transform/transform_left_join/transform_left_join_mt.hpp
+++ include/vison/transform/transform_left_join/transform_left_join_mt.hpp
@@ -23,6 +23,8 @@ void transform_left_join_mt(Dataframe &obj,
 
     const unsigned int& ncol2 = obj.get_ncol();
 
+    const unsigned int local_nrow = nrow;
+
     const std::vector<std::vector<unsigned int>>& matr_idx2 = obj.get_matr_idx();
 
     const auto& str_v2  = obj.get_str_vec();
@@ -39,66 +41,6 @@ void transform_left_join_mt(Dataframe &obj,
     const unsigned int size_uint = matr_idx[4].size();
     const unsigned int size_dbl  = matr_idx[5].size();
     
-    const std::vector<char>& vec_type = obj.get_typecol();
-
-    using key_variant_t = std::variant<
-        std::nullptr_t,
-        std::vector<std::vector<std::string>>*,
-        std::vector<std::vector<CharT>>*,
-        std::vector<std::vector<uint8_t>>*,
-        std::vector<std::vector<IntT>>*,
-        std::vector<std::vector<UIntT>>*,
-        std::vector<std::vector<FloatT>>*
-    >;
-    
-    key_variant_t pre_col1 = nullptr;
-    const key_variant_t pre_col2 = nullptr;
-    size_t idx_type;
-
-    if constexpr (!std::is_same_v<T, void>) {
-        if constexpr (std::is_same_v<T, std::string>)  {
-            pre_col1 = &str_v;
-            pre_col2 = &obj.get_str_v();
-            idx_type = 0;
-        } else if constexpr (std::is_same_v<T, CharT>) {
-            pre_col1 = &chr_v;
-            pre_col2 = &obj.get_chr_v();
-            idx_type = 1;
-        } else if constexpr (std::is_same_v<T, uint8_t>) {
-            pre_col1 = &bool_v;
-            pre_col2 = &obj.get_bool_v();
-            idx_type = 2;
-        } else if constexpr (std::is_same_v<T, IntT>) {
-            pre_col1 = &int_v;
-            pre_col2 = &obj.get_int_v();
-            idx_type = 3;
-        } else if constexpr (std::is_same_v<T, UIntT>) {
-            pre_col1 = &uint_v;
-            pre_col2 = &obj.get_uint_v();
-            idx_type = 4;
-        } else if constexpr (std::is_same_v<T, FloatT>) {
-            pre_col1 = &dbl_v;
-            pre_col2 = &obj.get_dbl_v();
-            idx_type = 5;
-        }
-    } else {
-        switch (type_refv[key1]) {
-            case 's': pre_col1 = &str_v;  idx_type = 0; pre_col2 = &obj.get_str_v();  break;
-            case 'c': pre_col1 = &chr_v;  idx_type = 1; pre_col2 = &obj.get_chr_v();  break;
-            case 'b': pre_col1 = &bool_v; idx_type = 2; pre_col2 = &obj.get_bool_v(); break;
-            case 'i': pre_col1 = &int_v;  idx_type = 3; pre_col2 = &obj.get_int_v();  break;
-            case 'u': pre_col1 = &uint_v; idx_type = 4; pre_col2 = &obj.get_uint_v(); break;
-            case 'd': pre_col1 = &dbl_v;  idx_type = 5; pre_col2 = &obj.get_dbl_v();  break;
-        }
-    }
-
-    size_t cur_idx = 0;
-    while (key1 != matr_idx[idx_type][cur_idx]) ++cur_idx;
-    const auto& col1 = (*pre_col1)[cur_idx];
-    cur_idx = 0;
-    while (key2 != matr_idx2[idx_type][cur_idx]) ++cur_idx;
-    const auto& col2 = (*pre_col2)[cur_idx];
-   
     str_v.resize(str_v.size()   + size_str);
     chr_v.resize(chr_v.size()   + size_chr);
     bool_v.resize(bool_v.size() + size_bool);
@@ -107,12 +49,12 @@ void transform_left_join_mt(Dataframe &obj,
     dbl_v.resize(dbl_v.size()   + size_dbl);
 
     for (size_t i = 0; i < ncol2; ++i) {
-        str_v [ncol + i].resize(nrow, default_str);
-        chr_v [ncol + i].resize(nrow, default_chr);
-        bool_v[ncol + i].resize(nrow, default_bool);
-        int_v [ncol + i].resize(nrow, default_int);
-        uint_v[ncol + i].resize(nrow, default_uint);
-        dbl_v [ncol + i].resize(nrow, default_dbl);
+        str_v [ncol + i].resize(local_nrow, default_str);
+        chr_v [ncol + i].resize(local_nrow, default_chr);
+        bool_v[ncol + i].resize(local_nrow, default_bool);
+        int_v [ncol + i].resize(local_nrow, default_int);
+        uint_v[ncol + i].resize(local_nrow, default_uint);
+        dbl_v [ncol + i].resize(local_nrow, default_dbl);
     }
 
     std::vector<std::vector<unsigned int>> matr_idx2b = matr_idx2;
@@ -153,112 +95,171 @@ void transform_left_join_mt(Dataframe &obj,
         >;
 
     map_t lookup;
-    lookup.reserve(col2.size());
 
-    std::vector<size_t> match_idx(nrow, SIZE_MAX);
-    const unsigned int nrow2;
-    if constexpr (Method == LeftJoinMethods::First || 
-                  Method == LeftJoinMethods::Last) {
 
-        if constexpr (std::is_same_v<T, std::string>) {
-            if constexpr (Method == LeftJoinMethod::First) {
-                for (size_t i = 0; i < col2.size(); i += 1) {
-                    lookup.try_emplace(col2[i], i);
-                };
-            } else {
-                for (size_t i = 0; i < col2.size(); i += 1) {
-                    lookup[col2[i]] = i;
-                };
-            }
-        } else {
-            if (idx_type != 0) {
-                constexpr auto& size_table = get_types_size();
-                const size_t val_size = size_table[idx_type];
-                if constexpr (Method == LeftJoinMethod::First) {
-                    for (size_t i = 0; i < col2.size(); i += 1) {
-                        lookup.try_emplace(std::string_view{reinterpret_cast<const char*>(&col2[i]), 
-                                                                val_size}, i);
-                    };
-                } else {
-                    for (size_t i = 0; i < col2.size(); i += 1) {
-                        lookup[std::string_view{reinterpret_cast<const char*>(&col2[i]), 
-                                                                val_size}] = i;
-                    };
-                }
-            } else {
-                if constexpr (Method == LeftJoinMethod::First) {
-                    for (size_t i = 0; i < col2.size(); i += 1) {
-                        lookup.try_emplace(col2[i], i);
-                    };
-                } else {
-                    for (size_t i = 0; i < col2.size(); i += 1) {
-                        lookup[col2[i]] = i;
-                    };
-                }
-            }
+    using key_variant_t = std::variant<
+        std::monostate,
+        std::vector<std::vector<std::string>>*,
+        std::vector<std::vector<CharT>>*,
+        std::vector<std::vector<uint8_t>>*,
+        std::vector<std::vector<IntT>>*,
+        std::vector<std::vector<UIntT>>*,
+        std::vector<std::vector<FloatT>>*
+    >; 
+    key_variant_t var_key_table1;
+    key_variant_t var_key_table2;
+    size_t idx_type;
+
+    if constexpr (!std::is_same_v<T, void>) {
+        if constexpr (std::is_same_v<element_type_t<T>, std::string>)  {
+            var_key_table1 = &str_v;
+            var_key_table2 = &str_v2;
+            idx_type = 0;
+        } else if constexpr (std::is_same_v<element_type_t<T>, CharT>) {
+            var_key_table1 = &chr_v;
+            var_key_table2 = &chr_v2;
+            idx_type = 1;
+        } else if constexpr (std::is_same_v<element_type_t<T>, uint8_t>) {
+            var_key_table1 = &bool_v;
+            var_key_table2 = &bool_v2;
+            idx_type = 2;
+        } else if constexpr (std::is_same_v<element_type_t<T>, IntT>) {
+            var_key_table1 = &int_v;
+            var_key_table2 = &int_v2;
+            idx_type = 3;
+        } else if constexpr (std::is_same_v<element_type_t<T>, UIntT>) {
+            var_key_table1 = &uint_v;
+            var_key_table2 = &uint_v2;
+            idx_type = 4;
+        } else if constexpr (std::is_same_v<element_type_t<T>, FloatT>) {
+            var_key_table1 = &dbl_v;
+            var_key_table2 = &dbl_v2;
+            idx_type = 5;
         }
-
-        nrow2 = obj.get_nrow();
-
-        for (size_t i = 0; i < nrow; ++i) {
-            auto it = lookup.find(col1[i]);
-            if (it != lookup.end())
-                match_idx[i] = it->second;
+    } else {
+        switch (type_refv[key1]) {
+            case 's': var_key_table1 = &str_v;  var_key_table2 = &str_v2;  idx_type = 0; break;
+            case 'c': var_key_table1 = &chr_v;  var_key_table2 = &chr_v2;  idx_type = 1; break;
+            case 'b': var_key_table1 = &bool_v; var_key_table2 = &bool_v2; idx_type = 2; break;
+            case 'i': var_key_table1 = &int_v;  var_key_table2 = &int_v2;  idx_type = 3; break;
+            case 'u': var_key_table1 = &uint_v; var_key_table2 = &uint_v2; idx_type = 4; break;
+            case 'd': var_key_table1 = &dbl_v;  var_key_table2 = &dbl_v2;  idx_type = 5; break;
         }
-
-    } else if constexpr (Method == LeftJoinMethods::Aligned) {
-
-        if constexpr (std::is_same_v<T, std::string>) {
-            for (size_t i = 0; i < col2.size(); i += 1) {
-                auto [it, inserted] = lookup.try_emplace(col2[i], MatchGroup{});
-                if (inserted) {
-                    it->second.idxs.reserve(3);
-                }
-                it->second.idxs.push_back(i);
-            };
-        } else {
-            if (idx_type != 0) {
-                constexpr auto& size_table = get_types_size();
-                const size_t val_size = size_table[idx_type];
-                for (size_t i = 0; i < col2.size(); i += 1) {
-                    auto [it, inserted] = lookup.try_emplace(std::string_view{reinterpret_cast<const char*>(&col2[i]), 
-                                                                 val_size}, MatchGroup{});
-                    if (inserted) {
-                        it->second.idxs.reserve(3);
-                    }
-                    it->second.idxs.push_back(i);
-                };
-            } else {
-                for (size_t i = 0; i < col2.size(); i += 1) {
-                    auto [it, inserted] = lookup.try_emplace(col2[i], MatchGroup{});
-                    if (inserted) {
-                        it->second.idxs.reserve(3);
-                    }
-                    it->second.idxs.push_back(i);
-                };
-            }
-        }
-
-        nrow2 = obj.get_nrow();
-
-        for (size_t i = 0; i < nrow; ++i) {
-            auto it = lookup.find(col1[i]);
-            if (it != lookup.end() && it->second.next < it->second.idxs.size()) {
-                match_idx[i] = it->second.idxs[it->second.next];
-                ++it->second.next;
-            }
-        }
-
     }
 
-    auto copy_block = [&](auto& dst_v, auto& src_v, size_t size_offset, size_t type_idx) {
-        for (size_t t = 0; t < matr_idx2[type_idx].size(); ++t) {
+    size_t table_idx1;
+    size_t table_idx2;
+
+    table_idx1 = 0;
+    table_idx2 = 0;
+    while (key1 != matr_idx[idx_type][cur_idx]) ++cur_idx1;
+    while (key2 != matr_idx2[idx_type][cur_idx]) ++cur_idx2;
+
+    std::vector<size_t> match_idx(local_nrow, SIZE_MAX);
+
+    std::visit([&lookup, 
+                &match_idx, 
+                &ob,
+                &var_key_table2,
+                val_size, 
+                table_idx1,
+                table_idx2](auto&& key_table) {
+
+        using TP = std::decay_t<decltype(key_table1)>;
+
+        if constexpr (!std::is_same_v<TP, std::monostate>) {
+
+            using Elem = TP::value_type::value_type;
+
+            const auto& col1 = (*key_table)[table_idx1];
+            const auto& col2 = (*std::get<TP>(var_key_table2))[table_idx2];
+
+            lookup.reserve(col2.size());
+
+            if constexpr (Method == LeftJoinMethods::First || 
+                          Method == LeftJoinMethods::Last) {
+
+                if constexpr (std::is_same_v<Elem, std::string>) {
+                    if constexpr (Method == LeftJoinMethod::First) {
+                        for (size_t i = 0; i < col2.size(); i += 1) {
+                            lookup.try_emplace(col2[i], i);
+                        };
+                    } else {
+                        for (size_t i = 0; i < col2.size(); i += 1) {
+                            lookup[col2[i]] = i;
+                        };
+                    }
+                } else {
+                   constexpr auto& size_table = get_types_size();
+                   const size_t val_size = size_table[idx_type];
+                   if constexpr (Method == LeftJoinMethod::First) {
+                       for (size_t i = 0; i < col2.size(); i += 1) {
+                           lookup.try_emplace(std::string_view{reinterpret_cast<const char*>(&col2[i]), 
+                                                                   val_size}, i);
+                       };
+                   } else {
+                       for (size_t i = 0; i < col2.size(); i += 1) {
+                           lookup[std::string_view{reinterpret_cast<const char*>(&col2[i]), 
+                                                                   val_size}] = i;
+                       };
+                   }
+                }
+
+                const unsigned int nrow2 = obj.get_nrow();
+
+                for (size_t i = 0; i < local_nrow; ++i) {
+                    auto it = lookup.find(col1[i]);
+                    if (it != lookup.end())
+                        match_idx[i] = it->second;
+                }
+
+            } else if constexpr (Method == LeftJoinMethods::Aligned) {
+
+                if constexpr (std::is_same_v<Elem, std::string>) {
+                    for (size_t i = 0; i < col2.size(); i += 1) {
+                        auto [it, inserted] = lookup.try_emplace(col2[i], MatchGroup{});
+                        if (inserted) {
+                            it->second.idxs.reserve(3);
+                        }
+                        it->second.idxs.push_back(i);
+                    };
+                } else {
+                    constexpr auto& size_table = get_types_size();
+                    const size_t val_size = size_table[idx_type];
+                    for (size_t i = 0; i < col2.size(); i += 1) {
+                        auto [it, inserted] = lookup.try_emplace(std::string_view{reinterpret_cast<const char*>(&col2[i]), 
+                                                                     val_size}, MatchGroup{});
+                        if (inserted) {
+                            it->second.idxs.reserve(3);
+                        }
+                        it->second.idxs.push_back(i);
+                    };
+                }
+
+                const unsigned int nrow2 = obj.get_nrow();
+
+                for (size_t i = 0; i < local_nrow; ++i) {
+                    auto it = lookup.find(col1[i]);
+                    if (it != lookup.end() && it->second.next < it->second.idxs.size()) {
+                        match_idx[i] = it->second.idxs[it->second.next];
+                        ++it->second.next;
+                    }
+                }
+
+            }
+
+        }
+
+    }, var_key_table1);
+
+    auto copy_block = [&matr_idx, &matr_idx2](auto& dst_v, auto& src_v, size_t size_offset, size_t idx_type) {
+        for (size_t t = 0; t < matr_idx2[idx_type].size(); ++t) {
     
             auto* dst_val = dst_v[size_offset + t].data();
             const auto* src_val = src_v[t].data();
    
             #pragma omp parallel for if(CORES > 1) num_threads(CORES)
-            for (size_t i = 0; i < nrow; ++i) {
+            for (size_t i = 0; i < local_nrow; ++i) {
                 size_t j = match_idx[i];
                 if (j != SIZE_MAX) {
                     dst_val[i] = src_val[j];
@@ -273,6 +274,8 @@ void transform_left_join_mt(Dataframe &obj,
     copy_block(int_v,  int_v2,  size_int,  3);
     copy_block(uint_v, uint_v2, size_uint, 4);
     copy_block(dbl_v,  dbl_v2,  size_dbl,  5); 
+
+    const std::vector<char>& vec_type = obj.get_typecol();
 
     type_refv.insert(type_refv.end(), vec_type.begin(), vec_type.end());
     ncol += ncol2;
