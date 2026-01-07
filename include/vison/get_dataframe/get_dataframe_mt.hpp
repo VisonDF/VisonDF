4dc1677ad77f44e47faede37d00e4b7fb7c63280
Julien <j.larget-piet@proton.me>
2026-01-07 16:51:21 +0100

    fixed matr_idx_map copy in get_dataframe...

--- include/vison/get_dataframe/get_dataframe_mt.hpp
+++ include/vison/get_dataframe/get_dataframe_mt.hpp
@@ -9,8 +9,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
     in_view                = cur_obj.get_in_view();
     row_view_idx           = cur_obj.get_row_view_idx();
     row_view_map           = cur_obj.get_row_view_map();
-    col_alrd_materialized  = cur_obj.get_col_alrd_materialized();
     const unsigned int local_nrow = nrow;
+    ankerl::unordered_dense::set<unsigned int>& pre_col_alrd_materialized  = cur_obj.get_col_alrd_materialized();
 
     auto str_copy_col = [local_nrow](std::string* dst, const std::string* src) {
 
@@ -112,10 +112,15 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
 
     if (cols.empty()) {
 
+        col_alrd_materialized  = pre_col_alrd_materialized();
         matr_idx     = cur_obj.get_matr_idx();
         matr_idx_map = cur_obj.get_matr_idx_map();
         sync_map_col = cur_obj.get_sync_map_col();
         ncol         = cur_obj.get_ncol();
+
+        if (in_view)
+            col_ard_materialized.reserve(ncol);
+
 
         const auto& str_v2  = cur_obj.get_str_vec();
         for (auto& el : str_v2) {
@@ -178,6 +183,9 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
     else {
         ncol = cols.size();
 
+        if (in_view)
+            col_ard_materialized.reserve(ncol);
+
         const auto& str_vec2  = cur_obj.get_str_vec();
         const auto& chr_vec2  = cur_obj.get_chr_vec();
         const auto& bool_vec2 = cur_obj.get_bool_vec();
@@ -198,8 +206,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
 
             switch (type_refv1[i]) {
                 case 's': {
+                               matr_idx_map[0][i2] = matr_idx[0].size();
                                matr_idx[0].push_back(i2);
-                               matr_idx_map[0] = i2;
                                str_v.emplace_back();
                                str_v.back().resize(local_nrow);
 
@@ -211,8 +219,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
                 case 'c': {
+                               matr_idx_map[1][i2] = matr_idx[1].size();
                                matr_idx[1].push_back(i2);
-                               matr_idx_map[1] = i2;
                                chr_v.emplace_back();
                                chr_v.back().resize(local_nrow);                               
 
@@ -224,8 +232,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
                 case 'b': {
+                               matr_idx_map[2][i2] = matr_idx[2].size();
                                matr_idx[2].push_back(i2);
-                               matr_idx_map[2] = i2;
                                bool_v.emplace_back();
                                bool_v.back().resize(local_nrow);                               
 
@@ -237,8 +245,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
                 case 'i': {
+                               matr_idx_map[3][i2] = matr_idx[3].size();
                                matr_idx[3].push_back(i2);
-                               matr_idx_map[3] = i2;
                                int_v.emplace_back();
                                int_v.back().resize(local_nrow); 
 
@@ -250,8 +258,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
                 case 'u': {
+                               matr_idx_map[4][i2] = matr_idx[4].size();
                                matr_idx[4].push_back(i2);
-                               matr_idx_map[4] = i2;
                                uint_v.emplace_back();
                                uint_v.back().resize(local_nrow);                               
 
@@ -263,8 +271,8 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
                 case 'd': {
+                               matr_idx_map[5][i2] = matr_idx[5].size();
                                matr_idx[5].push_back(i2);
-                               matr_idx_map[5] = i2;
                                dbl_v.emplace_back();
                                dbl_v.back().resize(local_nrow);                               
 
@@ -276,6 +284,9 @@ void get_dataframe_mt(const std::vector<size_t>& cols,
                                break;
                           }
             }
+
+            if (pre_col_alrd_materialized.contains(i))
+                col_alrd_materialized.insert(i);
 
             name_v[i2]    = name_v1[i];
             type_refv[i2] = type_refv1[i];
