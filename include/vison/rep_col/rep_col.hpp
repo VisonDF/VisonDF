4ec72c484a14d8fe5740e17866046ce7c2630d78
Julien <j.larget-piet@proton.me>
2026-01-16 14:25:36 +0100

    updated rep_col

--- include/vison/rep_col/rep_col.hpp
+++ include/vison/rep_col/rep_col.hpp
@@ -1,123 +1,188 @@
 #pragma once
 
-template <typename T, bool Large = false, bool IsBool = false> 
-void rep_col(std::vector<T> &x, unsigned int &colnb) {
+template <unsigned int CORES           = 4,
+          bool NUMA                    = false,
+          bool IsBool                  = false,
+          bool MapCol                  = false,
+          AssertionType AssertionLevel = AssertionType::None,
+          typename T> 
+void rep_col_mt(std::vector<T> &x, unsigned int &colnb) {
+
+    static_assert(is_supported_type<element_type<T>>, "Error, type not supported\n");
 
     const unsigned int local_nrow = nrow;
 
-    if (x.size() != local_nrow) {
-      std::cerr << "Error: vector length (" << x.size()
-                << ") does not match nrow (" << nrow << ")\n";
-      return;
+    if constexpr (AssertionLevel > AssertionType::None) {
+        if (x.size() != local_nrow) {
+          throw std::runtime_error("Vector out of bound\n");
+        }
     }
  
-    auto find_col_index = [&](auto& idx_vec) -> size_t {
-        size_t pos = 0;
-        while (pos < idx_vec.size() && idx_vec[pos] != colnb)
-            pos++;
-        if (pos == idx_vec.size()) {
-            std::cerr << "Error: column " << colnb
-                      << " not found for this type in (replace_col)\n";
-            std::terminate();
+    auto find_col_base = [x]([[maybe_unused]] const auto &idx_vec, 
+                             [[maybe_unused]] const size_t idx_type) -> size_t {
+        size_t pos;
+        if constexpr (!MapCol) {
+            pos = 0;
+            while (pos < idx_vec.size() && idx_vec[pos] != x)
+                ++pos;
+            if (pos == idx_vec.size()) {
+                throw std::runtime_error("Error in (get_col), no column found\n");
+            }
+        } else {
+            if (!matr_idx_map[idx_type].contains(x)) {
+                throw std::runtime_error("MapCol chosen but col not found in map\n");
+            }
+            if (!sync_map_col[idx_type]) {
+                throw std::runtime_error("Map not synced\n");
+            }
+            pos = matr_idx_map[idx_type][x];
         }
+
         return pos;
     };
 
-    auto replace_numeric = [&](auto& col_vec, auto& idx_vec) {
+    auto replace_pod = [&find_col_index,
+                        local_nrow,
+                        &x](
+                            T* dst 
+                           ) 
+    {
     
-        using U = std::decay_t<decltype(col_vec[0])>;     // column type
-        constexpr size_t buf_size = max_chars_needed<U>();
-    
-        size_t pos  = find_col_index(idx_vec);
-        //size_t base = pos * nrow;
-        U* dst = col_vec[pos].data();
-    
-        auto& val_tmp = tmp_val_refv[colnb];
-        for (auto& s: val_tmp)
-            s.reserve(buf_size);
-    
-        memcpy(dst, 
-               x.data(), 
-               local_nrow * sizeof(T));
-    
-        // convert x[i] → string view
-        for (size_t i = 0; i < local_nrow; ++i) {
-            char buf[buf_size];
-            auto [ptr, ec] = fast_to_chars(buf, buf + buf_size, x[i]);
-            if (ec == std::errc{}) {
-                val_tmp[i].assign(buf, ptr);
-            } else {
-                std::terminate();
-            }
-        }
-    };
+        if constexpr (CORES > 1) {
 
-    auto replace_string = [&](){
-    
-        size_t pos = find_col_index(matr_idx[0]);
-        std::string* dst = str_v[pos].data();
-        auto& val_tmp = tmp_val_refv[colnb];
-    
-        if constexpr (Large) {
-            for (size_t i = 0; i < local_nrow; ++i)
-                dst[i] = x[i];
-            for (size_t i = 0; i < local_nrow; ++i)
-                val_tmp[i] = x[i];
-        } else {
-            for (size_t i = 0; i < local_nrow; ++i) {
-                dst[i] = x[i];
-                val_tmp[i] = x[i];
-            }
-        }
-    };
+            if (CORES > local_nrow)
+                throw std::runtime_error("Too much cores for so little nrows\n");
 
-    auto replace_charbuf = [&]() {
-    
-        size_t pos = find_col_index(matr_idx[1]);
-        CharT* dst = chr_v[pos].data();
-        auto& val_tmp = tmp_val_refv[colnb];
-    
-        if constexpr (!Large) { 
-            for (size_t i = 0; i < local_nrow; ++i) {
-                dst[i] = x[i]; 
-                val_tmp[i].assign(x[i], df_charbuf_size);
+            int numa_nodes = 1;
+            if (numa_available() >= 0) 
+                numa_nodes = numa_max_node() + 1;
+
+            #pragma omp parallel num_threads(CORES)
+            {
+
+                const int tid        = omp_get_thread_num();
+                const int nthreads   = omp_get_num_threads();
+           
+                MtStruct cur_struct;
+
+                if constexpr (NUMA) {
+                    numa_mt(cur_struct,
+                            local_nrow, 
+                            tid, 
+                            nthreads, 
+                            numa_nodes);
+                } else {
+                    simple_mt(cur_struct,
+                              local_nrow, 
+                              tid, 
+                              nthreads);
+                }
+                    
+                const unsigned int start = cur_struct.start;
+                const unsigned int len   = cur_struct.len;
+
+                memcpy(dst + start, 
+                       x.data() + start, 
+                       len * sizeof(T));
+
             }
+
         } else {
+
             memcpy(dst, 
-                   x.data(),
-                   local_nrow * sizeof(CharT);
-            for (size_t i = 0; i < local_nrow; ++i)
-                val_tmp[i].assign(x[i], df_charbuf_size);
+                   x.data(), 
+                   local_nrow * sizeof(T));
+
         }
+    
+    };
+
+    auto replace_str = [this, 
+                        &find_col_index,
+                        local_nrow,
+                        &x](std::vector<std::string>& dst)
+    {
+
+        if constexpr (CORES > 1) {
+
+            if (CORES > local_nrow)
+                throw std::runtime_error("Too much cores for so little nrows\n");
+
+            int numa_nodes = 1;
+            if (numa_available() >= 0) 
+                numa_nodes = numa_max_node() + 1;
+
+            #pragma omp parallel num_threads(CORES)
+            {
+
+                const int tid        = omp_get_thread_num();
+                const int nthreads   = omp_get_num_threads();
+           
+                MtStruct cur_struct;
+
+                if constexpr (NUMA) {
+                    numa_mt(cur_struct,
+                            local_nrow, 
+                            tid, 
+                            nthreads, 
+                            numa_nodes);
+                } else {
+                    simple_mt(cur_struct,
+                              local_nrow, 
+                              tid, 
+                              nthreads);
+                }
+                    
+                const unsigned int start = cur_struct.start;
+                const unsigned int len   = cur_struct.len;
+
+                for (size_t i = start; i < start + len; ++i)
+                    dst[i] = x[i];
+
+            }
+
+        } else {
+            dst[pos] = x;
+        }
+    
     };
 
     if constexpr (IsBool) {
 
-        replace_numeric(bool_v,  matr_idx[2]);
+        const size_t pos_base = find_col_base(matr_idx[2], 2);
+        replace_pod(bool_v[pos_base].data());
 
-    } else if constexpr (std::is_same_v<T, IntT>) {
+    } else if constexpr (std::is_same_v<element_type_t<T>, IntT>) {
 
-        replace_numeric(int_v,  matr_idx[3]);
+        const size_t pos_base = find_col_base(matr_idx[3], 3);
+        replace_pod(int_v[pos_base].data());
 
-    } else if constexpr (std::is_same_v<T, UIntT>) {
+    } else if constexpr (std::is_same_v<element_type_t<T>, UIntT>) {
 
-        replace_numeric(uint_v,  matr_idx[4]);
+        const size_t pos_base = find_col_base(matr_idx[4], 4);
+        replace_pod(uint_v[pos_base].data());
 
-    } else if constexpr (std::is_same_v<T, FloatT>) {
+    } else if constexpr (std::is_same_v<element_type_t<T>, FloatT>) {
 
-        replace_numeric(dbl_v,  matr_idx[5]);
+        const size_t pos_base = find_col_base(matr_idx[5], 5);
+        replace_pod(dbl_v[pos_base].data());
 
-    } else if constexpr (std::is_same_v<T, std::string>) {
+    } else if constexpr (std::is_same_v<element_type_t<T>, std::string>) {
 
-        replace_string();
+        const size_t pos_base = find_col_base(matr_idx[0], 0);
+        replace_str(str_v[pos_base]);
 
-    } else if constexpr (std::is_same_v<T, CharT>) {
+    } else if constexpr (std::is_same_v<element_type_t<T>, CharT>) {
 
-        replace_charbuf();
+        const size_t pos_base = find_col_base(matr_idx[1], 1);
+        replace_pod(chr_v[pos_base].data());
 
     } else {
-      std::cerr << "Error unsupported type in (replace_col)\n";
+
+        std::cerr << "Error unsupported type in (replace_col)\n";
+
     };
+
 };
 
 
