719c263f40f40c9c34bc17c2d2dbe6ab0600e3e9
Julien <j.larget-piet@proton.me>
2026-02-01 19:03:16 +0100

    updated rm_col

--- include/vison/rm_col/rm_col_range.hpp
+++ include/vison/rm_col/rm_col_range.hpp
@@ -3,205 +3,161 @@
 template <typename T                    = void,
            bool MapCol                  = false,
            bool MemClean                = false,
-           bool SortedDesc              = true,  // you can lie here
            AssertionType AssertionLevel = AssertionType::Simple
          >
-void rm_col_range(std::vector<unsigned int>& nbcol) {
+void rm_col_range_sametype(std::vector<unsigned int> cols) // better if sorted ascendly
+{
 
     static_assert(is_supported_type<element_type_t<T>>, "Type not supported");
 
     const unsigned int local_ncol = ncol;
     if constexpr (AssertionLevel > AssertionType::None) {
-        for (auto el : nbcol) {
-            if (el > local_ncol) {
+        for (const auto nbcol : cols) {
+            if (nbcol > local_ncol) {
                 throw std::runtime_error("Column number out of bound\n");
             }
         }
     }
 
-    if constexpr (!SortedDesc) {
-        std::sort(nbcol.begin(), nbcol.end());
-        std::reverse(nbcol.begin(), nbcol.end());
-    }
-
     size_t idx_in_type;
     size_t idx_type;
 
-    std::variant<std::monostate,
-                 std::vector::std::vector<std::string>&,
-                 std::vector::std::vector<CharT>&,
-                 std::vector::std::vector<uint8_t>&,
-                 std::vector::std::vector<IntT>&,
-                 std::vector::std::vector<UIntT>&,
-                 std::vector::std::vector<FloatT>&,
-                 > var_key_table;
-    ankerl::unordered_dense::map<char, unsigned int> map_idx_col;
+    using container_t = std::conditional_t<std::is_same_v<T, void>,
+                            std::variant<std::monostate,
+                             std::vector<std::string>*,
+                             std::vector<CharT>*,
+                             std::vector<uint8_t>*,
+                             std::vector<IntT>*,
+                             std::vector<UIntT>*,
+                             std::vector<FloatT>*
+                            >,
+                            std::vector<element_type_t<T>>*
+    >;
+
+    key_t var_container_table;
 
     if constexpr (!std::is_same_v<T, void>) {
 
         if constexpr (std::is_same_v<element_type_t<T>, std::string>) {
             idx_type = 0;
-            var_key_table = str_v;
+            var_container_table = str_v;
         } else if constexpr (std::is_same_v<element_type_t<T>, CharT>) {
             idx_type = 1;
-            var_key_table = chr_v;
+            var_container_table = chr_v;
         } else if constexpr (std::is_same_v<element_type_t<T>, uint8_t>) {
             idx_type = 2;
-            var_key_table = bool_v;
+            var_container_table = bool_v;
         } else if constexpr (std::is_same_v<element_type_t<T>, IntT>) {
             idx_type = 3;
-            var_key_table = int_v;
+            var_container_table = int_v;
         } else if constexpr (std::is_same_v<element_type_t<T>, UIntT>) {
             idx_type = 4;
-            var_key_table = uint_v;
+            var_container_table = uint_v;
         } else if constexpr (std::is_same_v<element_type_t<T>, FloatT>) {
             idx_type = 5;
-            var_key_table = dbl_v;
+            var_container_table = dbl_v;
         } else {
             std::cerr << "Impossible type\n";
             return;
         }
 
     } else {
-        map_idx_col.emplace('s', 0);
-        map_idx_col.emplace('c', 1);
-        map_idx_col.emplace('b', 2);
-        map_idx_col.emplace('i', 3);
-        map_idx_col.emplace('u', 4);
-        map_idx_col.emplace('d', 5);
+
+        switch (type_refv[cols[0]]) {
+            case 's': idx_type = 0; break;
+            case 'c': idx_type = 1; break;
+            case 'b': idx_type = 2; break;
+            case 'i': idx_type = 3; break;
+            case 'u': idx_type = 4; break;
+            case 'd': idx_type = 5; break;
+            default: throw std::runtime_error("Type unknown, can not exist\n");
+        }
+
     }
 
-    bool alrd = false;
+    for (int i = cols.size() - 1; i > -1; --i) {
 
-    for (auto cur_nbcol : nbcol) {
-
+        const unsigned int nbcol = cols[i];
 
         if constexpr (!MapCol) {
 
-            idx_type = map_idx_col[cur_nbcol];
-
             idx_in_type =
-                std::find(matr_idx[idx_type].begin(), matr_idx[idx_type].end(), cur_nbcol)
+                std::find(matr_idx[idx_type].begin(), matr_idx[idx_type].end(), nbcol)
                 - matr_idx[idx_type].begin();
 
         } else {
 
-            if (!matr_idx_map[idx_type].contains(cur_nbcol)) {
+            if (!matr_idx_map[idx_type].contains(nbcol)) {
                 std::cerr << "MapCol chosen, but no corresponding col found in the map\n";
                 return;
             }
 
-            if (!sync_map_col[idx_type] || alrd) {
+            if (!sync_map_col[idx_type]) {
                 std::cerr << "MapCol not synced\n";
                 return;
             }
 
-            alrd = true;
-
-            idx_type = matr_idx_map[idx_type][cur_nbcol];
+            idx_type = matr_idx_map[idx_type][nbcol];
 
         }
 
-        if (!name_v.empty()) { name_v.erase(name_v.begin() + cur_nbcol); }
-        type_refv.erase(type_refv.begin() + cur_nbcol);
-        matr_idx[idx_type].erase(matr_idx[type_i].begin() + idx_in_type);
-        matr_idx_map[idx_type].erase(cur_nbcol);
+        if (!name_v.empty()) { name_v.erase(name_v.begin() + nbcol); }
+        type_refv.erase(type_refv.begin() + nbcol);
 
-        if constexpr (!std::is_same_v<T, void>) {
+        matr_idx[idx_type].erase(matr_idx[idx_type].begin() + idx_in_type);
+        matr_idx_map[idx_type].erase(nbcol);
 
-            std::visit([idx_type](auto&& key_table) {
-                key_table.erase(key_table.begin()  + idx_in_type); 
-            }, var_key_table);
+        if constexpr (std::is_same_v<T, void>) {
+
+            std::visit([idx_type](auto&& vec) {
+
+                vec.erase(key_table.begin()  + idx_in_type); 
+
+            }, var_container_table);
 
         } else {
 
-            switch (type_i) {
-                case 0: {
-                            str_v.erase(str_v.begin()  + idx_in_type); 
-                            break;
-                        }
-                case 1: {
-                            chr_v.erase(chr_v.begin()  + idx_in_type); 
-                            break;
-                        }
-                case 2: {
-                            bool_v.erase(bool_v.begin() + idx_in_type); 
-                            break;
-                        }
-                case 3: {
-                            int_v.erase(int_v.begin()  + idx_in_type); 
-                            break;
-                        }
-                case 4: {
-                            uint_v.erase(uint_v.begin() + idx_in_type); 
-                            break;
-                        }
-                case 5: {
-                            dbl_v.erase(dbl_v.begin()  + idx_in_type); 
-                            break;
-                        }
-            }
-
-            for (auto& [k, v] : matr_idx_map[idx_type]) {
-                if (v > idx_in_type)
-                    v -= 1;
-            }
+            var_container_table.erase(var_container_table.begin()  + idx_in_type); 
 
         }
 
-    }
-
-    for (auto cur_nbcol : nbcol) {
-
+        for (auto& [k, v] : matr_idx_map[idx_type]) {
+            if (v > idx_in_type)
+                v -= 1;
+        }
         for (size_t i = 0; i < 6; ++i) {
             for (auto& el : matr_idx[i]) {
-                if (el > cur_nbcol)
+                if (el > nbcol)
                     el -= 1;
             }
-
             for (auto& [k, v] : matr_idx_map[i]) {
-                if (k > cur_nbcol)
+                if (k > nbcol)
                     k -= 1;
             }
-
         }
 
     }
 
-    if constexpr (!std::is_same_v<T, void>) {
-
+    if constexpr (MemClean) {
         name_v                .shrink_to_fit();
         type_refv             .shrink_to_fit();
         matr_idx[idx_type]    .shrink_to_fit();
         matr_idx_map[idx_type].shrink_to_fit();
 
-        std::visit([idx_type](auto&& key_table) {
-            key_table.shrink_to_fit(); 
-        }, var_key_table);
+        if constexpr (std::is_same_v<T, void>) {
 
-    } else {
+            std::visit([idx_type](auto&& vec) {
+                vec.shrink_to_fit(); 
+            }, var_container_table);
 
-        if constexpr (MemClean) {
-            name_v   .shrink_to_fit();
-            type_refv.shrink_to_fit();
+        } else {
 
-            for (auto& el : matr_idx)
-                el.shrink_to_fit();
+            var_container_table.shrink_to_fit();
 
-            for (auto& el : matr_idx_map)
-                el.shrink_to_fit();
-
-            str_v .shrink_to_fit();
-            chr_v .shrink_to_fit();
-            bool_v.shrink_to_fit();
-            int_v .shrink_to_fit();
-            uint_v.shrink_to_fit();
-            dbl_v .shrink_to_fit();
         }
-
     }
 
-    ncol -= nbcol.size();
+    ncol -= cols.size();
 }
 
 
